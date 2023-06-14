4d2300656156892b9e39baa8a4ce780af9416b71
gitdrive5 <121691578+gitdrive5@users.noreply.github.com>
2023-06-14 12:38:49 +0530

    Update send_file.py

--- handlers/send_file.py
+++ handlers/send_file.py
@@ -12,7 +12,7 @@ async def reply_forward(message: Message, file_id: int):
     try:
         await message.reply_text(
             f"**Here is Sharable Link of this File:**\n"
-            f"https://nvstech4.blogspot.com?fid=tgnvs_{str_to_b64(str(file_id))}\n\n"
+            f"https://nvstech4.blogspot.com?fid={str_to_b64(str(file_id))}\n\n"
             f"__To Retrive the Stored File, just open the link!__",
             disable_web_page_preview=True,
             reply_markup=InlineKeyboardMarkup(
